12fa381df674675b90fbad977609d60a9fb41319
Brayan Burgos <brayan.burgos@mail.escuelaing.edu.co>
2021-02-11 16:03:33 -0500

    first push

---
+++ src/main/java/edu/escuelaing/edu/AREP/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.edu.AREP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
